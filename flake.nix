e36be9b9231755b86911b7e8d5cb4d03a64a6b66
Alexander Holmbäck <alexander.holmback@gmail.com>
2023-10-11 15:18:09 +0200

    ngninx + https

--- flake.nix
+++ flake.nix
@@ -5,14 +5,34 @@
   };
 
   outputs = { self }: {
-    nixosModules.default = { config, lib, ... }: {
+    nixosModules.default = { config, lib, ... }:
+    let
+      inherit (lib) mkOption types mkIf;
+      cfg = config.ahbk;
+    in {
       options.ahbk = {
-        enable = lib.mkOption {
-          type = lib.types.bool;
+        enable = mkOption {
+          type = types.bool;
           default = false;
         };
       };
-      config = {};
+      config = mkIf cfg.enable {
+        environment = {
+          systemPackages = [ ];
+        };
+        services.nginx = {
+          enable = true;
+          virtualHosts."ahbk.ddns.net" = {
+            addSSL = true;
+            enableACME = true;
+            root = "/var/www/ahbk.ddns.net";
+          };
+        };
+        security.acme = {
+          acceptTerms = true;
+          defaults.email = "alxhbk@proton.me";
+        };
+      };
     };
   };
 }
