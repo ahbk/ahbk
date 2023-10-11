1d269e928cb8e9c1dbe4f887c515180927ba3ff4
Alexander Holmbäck <alexander.holmback@gmail.com>
2023-10-11 15:57:20 +0200

    installPhase

--- flake.nix
+++ flake.nix
@@ -2,9 +2,23 @@
   description = "ahbk";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self }: {
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      name = "ahbk";
+      src = self;
+      installPhase = ''
+        mkdir $out
+        cp -R ./public $out/
+        '';
+    };
+
     nixosModules.default = { config, lib, ... }:
     let
       inherit (lib) mkOption types mkIf;
@@ -18,14 +32,14 @@
       };
       config = mkIf cfg.enable {
         environment = {
-          systemPackages = [ ];
+          systemPackages = [ self.packages.${system}.default ];
         };
         services.nginx = {
           enable = true;
           virtualHosts."ahbk.ddns.net" = {
             addSSL = true;
             enableACME = true;
-            root = "/var/www/ahbk.ddns.net";
+            root = "${self.packages.${system}.default.out}/public";
           };
         };
         security.acme = {
